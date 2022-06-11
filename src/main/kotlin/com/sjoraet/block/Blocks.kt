3f4d8ec52066084e907593b1b3b8823adacaa781
Sjoraet <sjoraet@protonmain.com>
2022-06-11 19:30:43 +0200

    Make multislab not break instantly

--- src/main/kotlin/com/sjoraet/block/Blocks.kt
+++ src/main/kotlin/com/sjoraet/block/Blocks.kt
@@ -9,7 +9,7 @@ import net.minecraft.util.registry.Registry
 
 class Blocks {
     companion object {
-        var MULTI_SLAB = Pair(Identifier(MODID, "multi_slab"), MultiSlabBlock(AbstractBlock.Settings.of(Material.STONE, MapColor.STONE_GRAY)));
+        var MULTI_SLAB = Pair(Identifier(MODID, "multi_slab"), MultiSlabBlock(AbstractBlock.Settings.of(Material.STONE, MapColor.STONE_GRAY).strength(1.0F)));
 
         fun init() {
             Registry.register(Registry.BLOCK, MULTI_SLAB.first, MULTI_SLAB.second);
